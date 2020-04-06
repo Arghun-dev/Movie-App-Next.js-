7f378f6d3670fe25ec6748ea998ffc1ee89a4bc0
Arghun-dev <arghun.developer@gmail.com>
2020-04-06 21:23:47 +0430

    Created MovieList

--- movie-app/pages/index.js
+++ movie-app/pages/index.js
@@ -8,6 +8,36 @@ import Carousel from '../components/Carousel.component'
 import MovieList from '../components/MovieList.component'
 import Footer from '../components/Footer.component'
 
+const MOVIE_DATA = [
+  {
+    id: '1',
+    name: 'The Shawshank Redemption',
+    releaseYear: 1994,
+    description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+    rating: 4.8,
+    genre: 'drama',
+    image: 'https://m.media-amazon.com/images/M/MV5BNjQ2NDA3MDcxMF5BMl5BanBnXkFtZTgwMjE5NTU0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg'
+  },
+  {
+    id: '2',
+    name: 'The Dark Knight',
+    releaseYear: 2008,
+    description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    rating: 4.7,
+    genre: 'action, crime, drama',
+    image: 'https://lh3.googleusercontent.com/auIs5tjWlLYaFPGClZOJ7m5YVbnX6uBvz0X02r8TkwFKdzE53ww2MqWSS9gU0YNqoYwvpg'
+  },
+  {
+    id: '3',
+    name: 'Lord of the Rings',
+    releaseYear: 2004,
+    description: 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
+    rating: 4.9,
+    genre: 'adventure, drama, fantasy',
+    image: 'https://vignette.wikia.nocookie.net/lotr/images/8/87/Ringstrilogyposter.jpg/revision/latest/top-crop/width/360/height/450?cb=20070806215413'
+  }
+]
+
 const Home = () => (
   <div>
     <Head>
@@ -30,7 +60,7 @@ const Home = () => (
         <div className="col-lg-9">
           <Carousel />
           <div className="row">
-            <MovieList />
+            <MovieList movies={MOVIE_DATA} />
           </div>
         </div>
       </div>
